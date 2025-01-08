8f054efe2a0f39970e0f50d9438b4ea187f7b0c5
Johannes <johannes.gruendl@gmail.com>
2025-01-08 16:07:32 +0100

    [TASK] treesitter: add auto_install and make file more readable

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,30 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "terraform", "yaml", "bash", "dockerfile", "json", "ssh_config"},
-        auto_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+					"terraform",
+					"yaml",
+					"bash",
+					"dockerfile",
+					"json",
+					"ssh_config",
+					"rasi",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
